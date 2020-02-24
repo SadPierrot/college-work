e5418c51db2a94a74ad54fc39a70f7effbd91f4f
SadPierrot <mr.tretyak15.2@gmail.com>
2020-02-24 17:07:50 +0300

    Creating a college repository
    -Creating a repository architecture
    -Implementation of the first practical task in the direction of "System Programming".
    - Implementation of the second practical task in the direction of "System Programming".

---
+++ System Programming/Practical works/2 practical work (C).c
@@ -0,0 +1,47 @@
+//==============================================НОМЕР 1.2-10=============================================================
+// Вторая практическая работа. Задача: Написать программу на Си по варинатам (2-0-19)
+//=======================================================================================================================
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main() {
+	int f, f1, f2, eps, num, num1, num2, num3, fac;
+	double s, sum;
+	int err = scanf("%lf", &eps);
+	if (err < 0) {
+		perror("Ошибка ввода");
+	}
+	else if (err == 0) {
+		perror("Ожидалось число");
+	}
+
+	f = 1;
+	f1 = 1;
+	f2 = 0;
+	sum = 0;
+	num = 2;
+	num1 = 1;
+	num2 = 3;
+	num3 = 3;
+
+	do {
+		f = f1 + f2;
+		f1 = f2;
+		f2 = f; 
+		num1 = num1 * 16;
+		fac = fac * num;
+		num = num + 1; 
+		num2 = sqrt(num3);
+		sum = (f + num1 * num2) / fac; 
+		s = s + sum;
+	} while
+		(sum < eps);
+		err = printf("%lf", s);
+	if (err < 0) {
+		perror("Ошибка вывода");
+	}
+
+	return 0;
+
+}
