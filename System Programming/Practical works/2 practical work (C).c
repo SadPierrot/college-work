2f17868e5e34bf5a5916edacafd3be25ebd8dce2
SadPierrot <mr.tretyak15.2@gmail.com>
2020-02-27 09:18:39 +0300

    Bug fix

--- System Programming/Practical works/2 practical work (C).c
+++ System Programming/Practical works/2 practical work (C).c
@@ -6,44 +6,44 @@
 #include <stdio.h>
 #include <math.h>
 int main() {
-	int f, f1, f2, num, num1, num2, num3, fac;
-	double s, sum, eps;
-	int err = scanf("%lf", &eps);
-	if (err < 0) {
-		perror("Ошибка ввода");
-	}
-	else if (err == 0) {
-		perror("Ожидалось число");
-	}
+int f, f1, f2, num, num1, num3, fac;
+double s, sum, eps, num2;
+int err = scanf("%lf", &eps);
+if (err < 0) {
+	perror("Ошибка ввода");	
+}
+else if (err == 0) {
+	perror("Ожидалось число");
+}
 
-	f = 1;
-	f1 = 1;
-	f2 = 0;
-	sum = 0;
-	num = 2;
-	num1 = 1;
-	num2 = 3;
-	num3 = 3;
-	fac = 2; 
+f = 1;
+f1 = 1;
+f2 = 0;
+s = 0;
+sum = 0;
+num = 2;
+num1 = 1;
+num3 = 3;
+fac = 2;
 
-	do {
-		f = f1 + f2;
-		f1 = f2;
-		f2 = f; 
-		num1 = num1 * 16;
-		fac = fac * num;
-		num = num + 1; 
-		num2 = sqrt(num3);
-		num3 = num3 + 1; 
-		sum = (f * num1 * num2) / fac; 
-		s = s + sum;
-	} while
-		(sum < eps);
-		err = printf("%lf", s);
-	if (err < 0) {
-		perror("Ошибка вывода");
-	}
+do {
+	f = f1 + f2;
+	f1 = f2;
+	f2 = f;
+	num1 = num1 * 16;
+	fac = fac * num;
+	num = num + 1;
+	num2 = sqrt(num3);
+	num3 = num3 + 1;
+	sum = (f * num1 * num2) / fac;
+	s = s + sum;
+} while
+(sum < eps);
+err = printf("%lf", s);
+if (err < 0) {
+	perror("Ошибка вывода");
+}
 
-	return 0;
+return 0;
 
 }
